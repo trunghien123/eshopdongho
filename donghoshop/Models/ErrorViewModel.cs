18184a9392dfbc278be46d77204c675d89b993e8
trunghien123 <hienhuynh076@gmail.com>
2020-05-11 16:30:19 +0700

    initial

---
+++ donghoshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace donghoshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
